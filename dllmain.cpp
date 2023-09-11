62a17e87677859b8dd1eede3589cdc0662f318b2
Gregory Brutsche <gregory@mail.brutsches.local>
2023-09-11 00:57:21 -0400

    Make "Weapon-Power-Mod" cover "SB_LIMITBODY_MAX_WEAPONS" too.

--- dllmain.cpp
+++ dllmain.cpp
@@ -17,17 +17,17 @@ std::map<std::string, std::vector<std::string>> TYPE_MAPPING = {
     {"Reactor-Class-Mod", std::vector<std::string> {"SB_ERRORBODY_REACTOR_CLASS"}},
     {"Shield-Count-Mod", std::vector<std::string> {"SB_LIMITBODY_MAX_SHIELD"}},
     {"Engine-Power-Mod", std::vector<std::string> {"SB_LIMITBODY_EXCESS_POWER_ENGINE"}},
-    {"Weapon-Power-Mod", std::vector<std::string> {"SB_LIMITBODY_EXCESS_POWER_WEAPON"}},
+    {"Weapon-Power-Mod", std::vector<std::string> {"SB_LIMITBODY_EXCESS_POWER_WEAPON", "SB_LIMITBODY_MAX_WEAPONS"}},
 };
 
-std::map<std::string, std::string> SCAN_MAPPING = {
-    //{"GravDrive-Count-Mod", "7E ?? 48 8D 15 ?? ?? ?? ?? 48 8D 4D 50"}, // 7E == `jle`
-    {"LandingGear-Count-Mod", "75 ?? 48 8D 15 ?? ?? ?? ?? 48 8D 4D 50"}, // 75 == `jne`
-    {"Reactor-Count-Mod", "7E ?? 48 8D 15 ?? ?? ?? ?? 48 8D 4D 50"}, // 7E == `jle`
-    {"Reactor-Class-Mod", "75 ?? 48 8D 15 ?? ?? ?? ?? 48 8D 4D 50"}, // 75 == `jne`
-    {"Shield-Count-Mod", "7E ?? 48 8D 15 ?? ?? ?? ?? 48 8D 4D 50"}, // 7E == `jle`
-    {"Engine-Power-Mod", "7E ?? 48 8D 15 ?? ?? ?? ?? 48 8D 4D 30"}, // 7E == `jle`
-    {"Weapon-Power-Mod", "EB ?? 48 8D 15 ?? ?? ?? ?? 48 8D 4D 30"}, // EB == `jmp`. This one's unique as the 74 (`je`) 4 ops before (-11 bytes) this to EB (`jmp`).
+std::map<std::string, std::vector<std::string>> SCAN_MAPPING = {
+    //{"GravDrive-Count-Mod", {"7E ?? 48 8D 15 ?? ?? ?? ?? 48 8D 4D 50"}}, // 7E == `jle`
+    {"LandingGear-Count-Mod", {"75 ?? 48 8D 15 ?? ?? ?? ?? 48 8D 4D 50"}}, // 75 == `jne`
+    {"Reactor-Count-Mod", {"7E ?? 48 8D 15 ?? ?? ?? ?? 48 8D 4D 50"}}, // 7E == `jle`
+    {"Reactor-Class-Mod", {"75 ?? 48 8D 15 ?? ?? ?? ?? 48 8D 4D 50"}}, // 75 == `jne`
+    {"Shield-Count-Mod", {"7E ?? 48 8D 15 ?? ?? ?? ?? 48 8D 4D 50"}}, // 7E == `jle`
+    {"Engine-Power-Mod", {"7E ?? 48 8D 15 ?? ?? ?? ?? 48 8D 4D 30"}}, // 7E == `jle`
+    {"Weapon-Power-Mod", {"EB ?? 48 8D 15 ?? ?? ?? ?? 48 8D 4D 30", "7E ?? 48 8D 15 ?? ?? ?? ?? 48 8D 4D 50"}}, // EB == `jmp`. This one's unique as the 74 (`je`) 4 ops before (-11 bytes) this to EB (`jmp`).
 };
 
 extern "C" {
@@ -63,54 +63,57 @@ void DoInjection() {
     auto moduleName = GetExeFilename();
     Log("Found module name: " << moduleName);
 
-    Log("Doing AoB scan.");
-
-    auto addressesFound = ScanMemory(moduleName, SCAN_MAPPING[TARGET_NAME]); // 7E == `jle`
-    if (addressesFound.empty()) {
-        Log("AoB scan returned no results, aborting.");
-        return;
-    }
-
-    auto newBytes = StringToByteVector("EB"); // EB == `jmp`
-    Log("Found " << addressesFound.size() << " match(es).");
-
-    auto& validTypes = TYPE_MAPPING[TARGET_NAME];
+    const auto newBytes = StringToByteVector("EB"); // EB == `jmp`
     auto patchedCount = 0;
 
-    for (auto address : addressesFound) {
-        UINT64 addrBase  = (UINT64) address;
-        UINT32 leaOffset = UINT32(*((UINT32*) (address + 5)));// In short, move the ptr 5 bytes, and dereference the 4 bytes (the `lea` offset) as an int.
-        UINT64 strBegin  = addrBase + leaOffset + 10; // +9 to offset to the end of the `lea` op, +1 to skip the char count. They're null-terminated anyways.
-        auto typeStr     = std::string(reinterpret_cast<char*>(strBegin));
+    for (auto& pattern : SCAN_MAPPING[TARGET_NAME]) {
+        Log("Doing AoB scan.");
 
-        if (contains(validTypes, typeStr)) {
-            Log("Target address: " << std::uppercase << std::hex << addrBase);
-            //PrintNBytes(address, 13);
-            //Log("LEA offset: " << std::uppercase << std::hex << leaOffset);
-            //Log("String addr: " << std::uppercase << std::hex << strBegin);
-            //Log("Type string: " << typeStr);
+        auto addressesFound = ScanMemory(moduleName, pattern);
+        if (addressesFound.empty()) {
+            Log("AoB scan returned no results, aborting.");
+            return;
+        }
 
-            if (TARGET_NAME == "Weapon-Power-Mod") {
-                auto jeAddress = address - 11;
+        Log("Found " << addressesFound.size() << " match(es).");
 
-                if (*jeAddress != 0x74) {
-                    Log("Error finding `JE` for `Weapon-Power-Mod`. Expected `74`, found `" << std::uppercase << std::hex << *jeAddress << "`. Aborting.");
-                    continue;
+        auto& validTypes = TYPE_MAPPING[TARGET_NAME];
+
+        for (auto& address : addressesFound) {
+            UINT64 addrBase  = (UINT64) address;
+            UINT32 leaOffset = UINT32(*((UINT32*) (address + 5)));// In short, move the ptr 5 bytes, and dereference the 4 bytes (the `lea` offset) as an int.
+            UINT64 strBegin  = addrBase + leaOffset + 10; // +9 to offset to the end of the `lea` op, +1 to skip the char count. They're null-terminated anyways.
+            auto typeStr     = std::string(reinterpret_cast<char*>(strBegin));
+
+            if (contains(validTypes, typeStr)) {
+                Log("Target address: " << std::uppercase << std::hex << addrBase);
+                //PrintNBytes(address, 13);
+                //Log("LEA offset: " << std::uppercase << std::hex << leaOffset);
+                //Log("String addr: " << std::uppercase << std::hex << strBegin);
+                //Log("Type string: " << typeStr);
+
+                if (TARGET_NAME == "Weapon-Power-Mod" && typeStr == "SB_LIMITBODY_EXCESS_POWER_WEAPON") {
+                    auto jeAddress = address - 11;
+
+                    if (*jeAddress != 0x74) {
+                        Log("Error finding `JE` for `Weapon-Power-Mod`. Expected `74`, found `" << std::uppercase << std::hex << *jeAddress << "`. Aborting.");
+                        continue;
+                    }
+
+                    Log("JE offset (-11) address: " << std::uppercase << std::hex << (UINT64) jeAddress);
+
+                    DoWithProtect((BYTE*) jeAddress, 1, [newBytes, jeAddress]() {
+                        memcpy((BYTE*) jeAddress, newBytes.data(), newBytes.size());
+                    });
+                } else {
+                    DoWithProtect((BYTE*) address, 1, [newBytes, address]() {
+                        memcpy((BYTE*) address, newBytes.data(), newBytes.size());
+                    });
                 }
 
-                Log("JE offset (-11) address: " << std::uppercase << std::hex << (UINT64) jeAddress);
-
-                DoWithProtect((BYTE*) jeAddress, 1, [newBytes, jeAddress]() {
-                    memcpy((BYTE*) jeAddress, newBytes.data(), newBytes.size());
-                });
-            } else {
-                DoWithProtect((BYTE*) address, 1, [newBytes, address]() {
-                    memcpy((BYTE*) address, newBytes.data(), newBytes.size());
-                });
+                Log(typeStr << " patched.");
+                patchedCount++;
             }
-
-            Log(typeStr << " patched.");
-            patchedCount++;
         }
     }
 
