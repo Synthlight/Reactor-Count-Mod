9cf5aaad8d539f534af7a40bba0ba951fe579356
Gregory Brutsche <gregory@mail.brutsches.local>
2023-09-13 18:24:04 -0400

    Tidy/Cleanup.

--- dllmain.cpp
+++ dllmain.cpp
@@ -10,17 +10,17 @@
 
 std::ofstream out;
 
-std::map<std::string, std::vector<std::string>> TYPE_MAPPING = {
-    //{"GravDrive-Count-Mod", std::vector<std::string> {"SB_LIMITBODY_MAX_GRAV_DRIVE"}}, // Winds up with "you need additional grav thrust".
-    {"LandingGear-Count-Mod", std::vector<std::string> {"SB_LIMITBODY_MIN_LANDING_GEAR"}},
-    {"Reactor-Count-Mod", std::vector<std::string> {"SB_LIMITBODY_MAX_REACTOR"}},
-    {"Reactor-Class-Mod", std::vector<std::string> {"SB_ERRORBODY_REACTOR_CLASS"}},
-    {"Shield-Count-Mod", std::vector<std::string> {"SB_LIMITBODY_MAX_SHIELD"}},
-    {"Engine-Power-Mod", std::vector<std::string> {"SB_LIMITBODY_EXCESS_POWER_ENGINE"}},
-    {"Weapon-Power-Mod", std::vector<std::string> {"SB_LIMITBODY_EXCESS_POWER_WEAPON", "SB_LIMITBODY_MAX_WEAPONS"}},
+const std::map<std::string, std::vector<std::string>> TYPE_MAPPING = {
+    //{"GravDrive-Count-Mod", std::vector<std::string>{"SB_LIMITBODY_MAX_GRAV_DRIVE"}}, // Winds up with "you need additional grav thrust".
+    {"LandingGear-Count-Mod", std::vector<std::string>{"SB_LIMITBODY_MIN_LANDING_GEAR"}},
+    {"Reactor-Count-Mod", std::vector<std::string>{"SB_LIMITBODY_MAX_REACTOR"}},
+    {"Reactor-Class-Mod", std::vector<std::string>{"SB_ERRORBODY_REACTOR_CLASS"}},
+    {"Shield-Count-Mod", std::vector<std::string>{"SB_LIMITBODY_MAX_SHIELD"}},
+    {"Engine-Power-Mod", std::vector<std::string>{"SB_LIMITBODY_EXCESS_POWER_ENGINE"}},
+    {"Weapon-Power-Mod", std::vector<std::string>{"SB_LIMITBODY_EXCESS_POWER_WEAPON", "SB_LIMITBODY_MAX_WEAPONS"}},
 };
 
-std::map<std::string, std::vector<std::string>> SCAN_MAPPING = {
+const std::map<std::string, std::vector<std::string>> SCAN_MAPPING = {
     //{"GravDrive-Count-Mod", {"7E ?? 48 8D 15 ?? ?? ?? ?? 48 8D 4D 50"}}, // 7E == `jle`
     {"LandingGear-Count-Mod", {"75 ?? 48 8D 15 ?? ?? ?? ?? 48 8D 4D 50"}}, // 75 == `jne`
     {"Reactor-Count-Mod", {"7E ?? 48 8D 15 ?? ?? ?? ?? 48 8D 4D 50"}}, // 7E == `jle`
@@ -30,105 +30,96 @@ std::map<std::string, std::vector<std::string>> SCAN_MAPPING = {
     {"Weapon-Power-Mod", {"EB ?? 48 8D 15 ?? ?? ?? ?? 48 8D 4D 30", "7E ?? 48 8D 15 ?? ?? ?? ?? 48 8D 4D 50"}}, // EB == `jmp`. This one's unique as the 74 (`je`) 4 ops before (-11 bytes) this to EB (`jmp`).
 };
 
-extern "C" {
-    // Copied from `PluginAPI.h`.
-    __declspec(dllexport) SFSEPluginVersionData SFSEPlugin_Version = {
-        SFSEPluginVersionData::kVersion,
-
-        1,
-        TARGET_NAME,
-        "LordGregory",
-
-        0,	// not address independent
-        0,	// not structure independent
-        { CURRENT_RELEASE_RUNTIME, 0 },	// compatible with 1.7.23 and that's it
-
-        0,	// works with any version of the script extender. you probably do not need to put anything here
-        0, 0,	// set these reserved fields to 0
-    };
-};
-
-void PrintNBytes(const BYTE* address, const int length) {
-    out << NOW << '\t' << "Bytes: ";
-    for (auto i = 0; i < length; i++) {
-        if (i > 0) out << ",";
-        out << std::uppercase << std::hex << UINT32(*(address + i));
-    }
-    out << std::endl;
-}
-
 void DoInjection() {
-    Log(TARGET_NAME << " loading.");
+    LOG(TARGET_NAME << " loading.");
 
-    auto moduleName = GetExeFilename();
-    Log("Found module name: " << moduleName);
+    const auto moduleName = GetExeFilename();
+    LOG("Found module name: " << moduleName);
 
-    const auto newBytes = StringToByteVector("EB"); // EB == `jmp`
-    auto patchedCount = 0;
+    const auto newBytes     = StringToByteVector("EB"); // EB == `jmp`
+    auto       patchedCount = 0;
 
-    for (auto& pattern : SCAN_MAPPING[TARGET_NAME]) {
-        Log("Doing AoB scan.");
+    for (const auto& pattern : SCAN_MAPPING.at(TARGET_NAME)) {
+        LOG("Doing AoB scan.");
 
         auto addressesFound = ScanMemory(moduleName, pattern);
         if (addressesFound.empty()) {
-            Log("AoB scan returned no results, aborting.");
+            LOG("AoB scan returned no results, aborting.");
             return;
         }
 
-        Log("Found " << addressesFound.size() << " match(es).");
+        LOG("Found " << addressesFound.size() << " match(es).");
 
-        auto& validTypes = TYPE_MAPPING[TARGET_NAME];
+        auto validTypes = TYPE_MAPPING.at(TARGET_NAME);
 
-        for (auto& address : addressesFound) {
-            UINT64 addrBase  = (UINT64) address;
-            UINT32 leaOffset = UINT32(*((UINT32*) (address + 5)));// In short, move the ptr 5 bytes, and dereference the 4 bytes (the `lea` offset) as an int.
-            UINT64 strBegin  = addrBase + leaOffset + 10; // +9 to offset to the end of the `lea` op, +1 to skip the char count. They're null-terminated anyways.
-            auto typeStr     = std::string(reinterpret_cast<char*>(strBegin));
+        for (const auto& address : addressesFound) {
+            const auto   addrBase  = reinterpret_cast<const UINT64>(address);
+            const auto   leaOffset = *reinterpret_cast<const UINT32*>(address + 5); // In short, move the ptr 5 bytes, and dereference the 4 bytes (the `lea` offset) as an int. // NOLINT(clang-diagnostic-cast-qual)
+            const UINT64 strBegin  = addrBase + leaOffset + 10; // +9 to offset to the end of the `lea` op, +1 to skip the char count. They're null-terminated anyways.
+            const auto   typeStr   = std::string(reinterpret_cast<const char*>(strBegin)); // NOLINT(performance-no-int-to-ptr)
 
-            if (contains(validTypes, typeStr)) {
-                Log("Target address: " << std::uppercase << std::hex << addrBase);
+            if (Contains(validTypes, typeStr)) {
+                LOG("Target address: " << std::uppercase << std::hex << addrBase);
                 //PrintNBytes(address, 13);
-                //Log("LEA offset: " << std::uppercase << std::hex << leaOffset);
-                //Log("String addr: " << std::uppercase << std::hex << strBegin);
-                //Log("Type string: " << typeStr);
+                //LOG("LEA offset: " << std::uppercase << std::hex << leaOffset);
+                //LOG("String addr: " << std::uppercase << std::hex << strBegin);
+                //LOG("Type string: " << typeStr);
 
-                if (TARGET_NAME == "Weapon-Power-Mod" && typeStr == "SB_LIMITBODY_EXCESS_POWER_WEAPON") {
+                if (std::strcmp(TARGET_NAME, "Weapon-Power-Mod") != 0 && typeStr == "SB_LIMITBODY_EXCESS_POWER_WEAPON") {
                     auto jeAddress = address - 11;
 
                     if (*jeAddress != 0x74) {
-                        Log("Error finding `JE` for `Weapon-Power-Mod`. Expected `74`, found `" << std::uppercase << std::hex << *jeAddress << "`. Aborting.");
+                        LOG("Error finding `JE` for `Weapon-Power-Mod`. Expected `74`, found `" << std::uppercase << std::hex << *jeAddress << "`. Aborting.");
                         continue;
                     }
 
-                    Log("JE offset (-11) address: " << std::uppercase << std::hex << (UINT64) jeAddress);
+                    LOG("JE offset (-11) address: " << std::uppercase << std::hex << reinterpret_cast<const UINT64>(jeAddress));
 
-                    DoWithProtect((BYTE*) jeAddress, 1, [newBytes, jeAddress]() {
-                        memcpy((BYTE*) jeAddress, newBytes.data(), newBytes.size());
+                    DoWithProtect(const_cast<BYTE*>(jeAddress), 1, [newBytes, jeAddress] {
+                        memcpy(const_cast<BYTE*>(jeAddress), newBytes.data(), newBytes.size());
                     });
                 } else {
-                    DoWithProtect((BYTE*) address, 1, [newBytes, address]() {
-                        memcpy((BYTE*) address, newBytes.data(), newBytes.size());
+                    DoWithProtect(const_cast<BYTE*>(address), 1, [newBytes, address] {
+                        memcpy(const_cast<BYTE*>(address), newBytes.data(), newBytes.size());
                     });
                 }
 
-                Log(typeStr << " patched.");
+                LOG(typeStr << " patched.");
                 patchedCount++;
             }
         }
     }
 
-    Log("Patched " << patchedCount << " match(es).");
+    LOG("Patched " << patchedCount << " match(es).");
 }
 
 extern "C" {
-    __declspec(dllexport) void SFSEPlugin_Load(const SFSEInterface* sfse) {
-        out = SetupLog(GetLogPathAsCurrentDllDotLog());
+// Copied from `PluginAPI.h`.
+// ReSharper disable once CppInconsistentNaming
+__declspec(dllexport) SFSEPluginVersionData SFSEPlugin_Version = {
+    SFSEPluginVersionData::kVersion,
 
-        auto thread = std::thread([]() {
-            DoInjection();
-        });
-        if (thread.joinable()) thread.detach();
+    1,
+    TARGET_NAME,
+    "LordGregory",
 
-        Log("Scan thread spawned."); // Don't remove. Plugin fails to load without it for some mysterious reason.
-    }
+    0, // not address independent
+    0, // not structure independent
+    {CURRENT_RELEASE_RUNTIME, 0}, // compatible with 1.7.23 and that's it
+
+    0, // works with any version of the script extender. you probably do not need to put anything here
+    0, 0, // set these reserved fields to 0
+};
+
+// ReSharper disable once CppInconsistentNaming
+__declspec(dllexport) void SFSEPlugin_Load(const SFSEInterface* sfse) {
+    out = SetupLog(GetLogPathAsCurrentDllDotLog());
+
+    auto thread = std::thread([] {
+        DoInjection();
+    });
+    if (thread.joinable()) thread.detach();
+
+    LOG("Scan thread spawned."); // Don't remove. Plugin fails to load without it for some mysterious reason.
+}
 };
